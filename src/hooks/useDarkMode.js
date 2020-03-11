21531223556540119b8389164b104859e7632c52
Michael-B1764 <mikebailar@gmail.com>
2020-03-11 13:02:42 -0700

    I can now toggle local storage value darkModeEnalbed to true or false. Need to figure out how to get the colors to change.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,22 +1,22 @@
-import useLocalStorage from "./useLocalStorage"
+import {useLocalStorage} from "./useLocalStorage"
 import {useEffect} from "react"
 
-export default useDarkMode = (booleanValue) => {
+export const useDarkMode = darkModeBoolean => {
 
-    const [storedValue, setValue] = useLocalStorage(darkMode, booleanValue)
-    const body = document.querySelector("body")
+    const [storedValue, setValue] = useLocalStorage('darkModeEnabled', darkModeBoolean);
+    const body = document.querySelector("body");
 
-    const useEffect = () => {
+    useEffect(() => {
         //check use storage value
         if (storedValue === true) {
-            body.classList.remove("dark-mode")
+            body.classList.add("dark-mode");
         } else {
-            body.classList.add("dark-mode")
+            body.classList.remove("dark-mode");
         }
         
-    }, [booleanValue]
+    }, [darkModeBoolean]);
     
-    return [storedValue, setValue]
+    return [storedValue, setValue];
 
 }
 
